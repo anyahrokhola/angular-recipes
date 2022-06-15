ade021cecb34794a2ffaab6281faa0a584a5d60f
Misha Hrokhola <m.hrokhola@tenantcloud.com>
2022-06-15 12:42:31 +0300

    Added required packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
+import { DialogModule } from '@ngneat/dialog';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+
+import { NgxsModule } from '@ngxs/store';
+import { NgxsFormPluginModule } from '@ngxs/form-plugin';
+import { NgxsStoragePluginModule } from '@ngxs/storage-plugin';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+
 import { AppComponent } from './app.component';
 
+import { environment } from '../environments/environment';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+
+		DialogModule.forRoot(),
+
+		NgxsModule.forRoot([], {
+			developmentMode: !environment.production,
+			compatibility: { strictContentSecurityPolicy: true },
+		}),
+		NgxsFormPluginModule.forRoot(),
+		NgxsReduxDevtoolsPluginModule.forRoot({ disabled: environment.production }),
+		NgxsStoragePluginModule.forRoot({ key: ['forms'] }),
+	],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
