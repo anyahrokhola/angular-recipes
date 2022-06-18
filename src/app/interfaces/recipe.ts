17dfb665ce68cb94eecb26b0020dc032271fa3f4
Misha Hrokhola <m.hrokhola@tenantcloud.com>
2022-06-18 06:34:43 +0300

    Formatted code

--- src/app/interfaces/recipe.ts
+++ src/app/interfaces/recipe.ts
@@ -1,48 +1,48 @@
 import { Time } from '@angular/common';
 
 export interface Recipe {
-  category: Category;
-  description: RecipeStep[];
-  difficulty: Difficulty;
-  id: number;
-  img: string;
-  meal: Meal;
-  name: string;
-  products: ProductInRecipe;
-  time: Time;
+	category: Category;
+	description: RecipeStep[];
+	difficulty: Difficulty;
+	id: number;
+	img: string;
+	meal: Meal;
+	name: string;
+	products: ProductInRecipe;
+	time: Time;
 }
 
 export interface RecipeStep {
-  description: string;
-  id: number;
-  index: number;
+	description: string;
+	id: number;
+	index: number;
 }
 
 export interface ProductInRecipe {
-  count: number;
-  id: number;
-  unit: string;
+	count: number;
+	id: number;
+	unit: string;
 }
 
 export const enum Category {
-  FirstСourse,
-  SecondСourse,
-  SaladAndSnack,
-  Baking,
-  Cakes,
-  Desserts,
-  Drinks,
+	FirstСourse,
+	SecondСourse,
+	SaladAndSnack,
+	Baking,
+	Cakes,
+	Desserts,
+	Drinks,
 }
 
 export const enum Difficulty {
-  Easy,
-  Medium,
-  Hard,
+	Easy,
+	Medium,
+	Hard,
 }
 
 export const enum Meal {
-  Breakfast,
-  Lunch,
-  Dinner,
-  Snack,
+	Breakfast,
+	Lunch,
+	Dinner,
+	Snack,
 }
