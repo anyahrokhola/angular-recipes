67a7f9c8a55c8ae457d559ea70f85a5acbcf5281
Anna <karaihr6@gmail.com>
2022-07-03 10:18:46 +0300

    added footer

--- src/app/modules/pages/pages.module.ts
+++ src/app/modules/pages/pages.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HeaderComponent } from './components/header/header.component';
 import { AnimationButtonComponent } from './components/animation-button/animation-button.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SocialMediaComponent } from './components/social-media/social-media.component';
 
 @NgModule({
-	declarations: [HeaderComponent, AnimationButtonComponent],
+	declarations: [HeaderComponent, AnimationButtonComponent, FooterComponent, SocialMediaComponent],
 	imports: [BrowserModule, CommonModule],
-	exports: [HeaderComponent, AnimationButtonComponent],
+	exports: [HeaderComponent, AnimationButtonComponent, FooterComponent, SocialMediaComponent],
 })
 export class PagesModule {}
