ef670b07f21b86254efdc96365b0706531101b77
Anna <karaihr6@gmail.com>
2022-08-08 18:38:58 +0300

    added directive for view passwords

--- src/app/modules/pages/directives/password-togle/password-togle.directive.ts
+++ src/app/modules/pages/directives/password-togle/password-togle.directive.ts
@@ -8,14 +8,34 @@ export class PasswordTogleDirective implements AfterViewInit {
 	@Input() public visible!: boolean;
 	@Input() public changeType!: boolean;
 
-	public iconElement = document.createElementNS('', 'i');
+	public iconElement = document.createElement('i');
 
 	constructor(private elementRef: ElementRef<HTMLInputElement>) {}
 
 	public ngAfterViewInit(): void {
 		this.iconElement.setAttribute('class', 'fa-solid fa-eye');
+		this.iconElement.style.position = 'absolute';
+		this.iconElement.style.top = '0';
+		this.iconElement.style.right = '0';
 		this.elementRef.nativeElement.parentNode?.insertBefore(this.iconElement, this.elementRef.nativeElement);
-		console.log('this.iconElement', this.iconElement);
-		console.log(this.elementRef);
+		this.iconElement.addEventListener('click', () => {
+			this.visible = !this.visible;
+			this.changeType = !this.changeType;
+
+			this.viewPass();
+		});
+	}
+
+	public viewPass(): void {
+		if (this.visible) {
+			this.iconElement.setAttribute('class', 'fa-solid fa-eye-slash');
+		} else {
+			this.iconElement.setAttribute('class', 'fa-solid fa-eye');
+		}
+		if (this.changeType) {
+			this.elementRef.nativeElement.type = 'text';
+		} else {
+			this.elementRef.nativeElement.type = 'password';
+		}
 	}
 }
