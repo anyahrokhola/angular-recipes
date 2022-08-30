12d0373fdde4f2ea7bb10a03b491a57db59d7143
Anna <karaihr6@gmail.com>
2022-08-30 13:03:22 +0300

    created function for option service

--- src/app/services/option/option.service.ts
+++ src/app/services/option/option.service.ts
@@ -4,20 +4,25 @@ import { Category, Difficulty, Meal } from '../../models';
 import { categoryI18n } from 'src/app/i18n/category.i18n';
 import { difficultyI18n } from 'src/app/i18n/difficulty.i18n';
 import { mealI18n } from 'src/app/i18n/meal.i18n';
+import { map } from 'lodash';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class OptionService {
 	public getCategoryOptions(): Array<SelectOption<Category>> {
-		return Object.values(Category).map(el => ({ value: el, text: categoryI18n[el] }));
+		return OptionService.transformToSelectOptions(categoryI18n);
 	}
 
 	public getDifficultyOptions(): Array<SelectOption<Difficulty>> {
-		return Object.values(Difficulty).map(el => ({ value: el, text: difficultyI18n[el] }));
+		return OptionService.transformToSelectOptions(difficultyI18n);
 	}
 
 	public getMealOptions(): Array<SelectOption<Meal>> {
-		return Object.values(Meal).map(el => ({ value: el, text: mealI18n[el] }));
+		return OptionService.transformToSelectOptions(mealI18n);
+	}
+
+	public static transformToSelectOptions<E extends string | number>(data: Record<E, string>): Array<SelectOption<E>> {
+		return map(data, (text, enumValue) => ({ value: enumValue, text })) as Array<SelectOption<E>>;
 	}
 }
